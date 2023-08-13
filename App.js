184cf555b0acb52ddad045695fc24b6a385f9757
Muneer ahmad wani <63859657+MUNEERWANI@users.noreply.github.com>
2023-08-13 07:49:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+import SellerForm from './components/seller/SellerForm';
+function App() {
+  return (
+    <div>
+    <SellerForm />
+  </div>
+  );
+}
+
+export default App;
